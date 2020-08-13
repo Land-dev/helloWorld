0e064256c02b051f3d95facfb4cabb679a3ec45c
Land-dev <ryan@awkvi.com>
2020-08-12 19:54:22 -0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,25 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		
+		System.out.println("My name is Ryan");
+		
+	    System.out.println("I like to mountain bike, play tennis, and run cross country");
+	    
+	    System.out.println("I'm looking to major in engineering, and am really interested in aerospace engineering");
+	    
+	    System.out.println("However, I'm open to computer engineering as well, which is why I'm taking AP Comp Sci");
+	    
+	    System.out.println("If I can, I'd like to go to the east coast for college so I can escape from California and never come back");
+
+	}
+}
